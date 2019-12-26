1d35aa0bdddc74ea0040bd75709a099465a9e2aa
Firwing-fawn <59110834+Firwing-fawn@users.noreply.github.com>
2019-12-26 21:38:48 +0500

    Updates

---
+++ precache-manifest.8b059139f7b13852148061d4975db24d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a633308d048f80771fd68aad97c8f84",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f44c83f93e4015f9f55e",
+    "url": "./static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "51fb365f3d29e54bd0bf",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "f44c83f93e4015f9f55e",
+    "url": "./static/js/2.408c73d2.chunk.js"
+  },
+  {
+    "revision": "51fb365f3d29e54bd0bf",
+    "url": "./static/js/main.5d30fe75.chunk.js"
+  },
+  {
+    "revision": "5cadbb88c7ae18cd8b02",
+    "url": "./static/js/runtime-main.ea1f0619.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ferreat/precache-manifest.a57f7a6054b9294cb56792dbeaf60acb.js"
+  "./precache-manifest.8b059139f7b13852148061d4975db24d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ferreat/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
